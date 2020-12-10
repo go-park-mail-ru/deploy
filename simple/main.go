93dbe06c2989913cc5fe4af6c207fbb403f0c82d
Dmitry Dorofeev <d.dorofeev@corp.mail.ru>
2020-12-10 18:37:37 +0300

    add service restart

--- simple/main.go
+++ simple/main.go
@@ -13,13 +13,13 @@ func OilPriceHandler(w http.ResponseWriter, r *http.Request) {
 
 	magicNumber := rand.Int()
 
-	w.Write([]byte("Oil price: " + strconv.Itoa(magicNumber)))
+	w.Write([]byte(strconv.Itoa(magicNumber)))
 }
 
 func main() {
 	http.HandleFunc("/", OilPriceHandler)
 
 	fmt.Println("Starting service")
-	
+
 	http.ListenAndServe(":8080", nil)
 }
